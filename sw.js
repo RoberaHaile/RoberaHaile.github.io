9089286e8fd5415cb48c7addce107d8e7b4ae12d
RoberaHaile <robera555@gmail.com>
2021-09-13 18:32:28 +0300

    Sep_13_2021

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210913.1821';
+const cacheName = 'chirpy-20210913.1832';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
